63d3ac863a2997a84b3f77f573e228e8230a644a
Aditya <01fe23bcs152@kltech.ac.in>
2026-01-05 12:40:44 +0530

    message

--- backend/utils/vc_generator.py
+++ backend/utils/vc_generator.py
@@ -26,29 +26,7 @@ class VCGenerator:
         "https://w3id.org/security/suites/ed25519-2020/v1",
         {
             "academic": "https://credentials.kle.edu/academic#",
-            "Transcript": "academic:Transcript",
-            "Markscard": "academic:Markscard",
-            "CourseCompletion": "academic:CourseCompletion",
-            "Workshop": "academic:Workshop",
-            "Hackathon": "academic:Hackathon",
-            "semesters": "academic:semesters",
-            "courses": "academic:courses",
-            "course_code": "academic:course_code",
-            "course_name": "academic:course_name",
-            "credits": "academic:credits",
-            "grade": "academic:grade",
-            "gpa": "academic:gpa",
-            "sgpa": "academic:sgpa",
-            "cgpa": "academic:cgpa",
-            "cgpa_in_words": "academic:cgpa_in_words",
-            "result_class": "academic:result_class",
-            "year_of_completion": "academic:year_of_completion",
-            "total_credits": "academic:total_credits",
-            "date_of_issue": "academic:date_of_issue",
-            "program": "academic:program",
-            "branch": "academic:branch",
-            "semester": "academic:semester",
-            "father_or_mother_name": "academic:father_or_mother_name"
+            "@vocab": "https://credentials.kle.edu/academic#"
         }
     ]
     
@@ -143,8 +121,7 @@ class VCGenerator:
         
         # 1. Create Proof Template
         # Fragment MUST be the key identifier (the part after did:key:)
-        # Use standard #key-1 fragment for did:key
-        key_fragment = "key-1"
+        key_fragment = did.split(":")[-1]
         created_date = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
         
         proof = {
@@ -231,23 +208,10 @@ class VCGenerator:
         if issuer_info.get('credential_metadata'):
             subject.update(issuer_info['credential_metadata'])
 
-        type_list = ["VerifiableCredential"]
-        if credential_type:
-            # Map simple types to capitalized PascalCase for JSON-LD type
-            type_map = {
-                "transcript": "Transcript",
-                "markscard": "Markscard",
-                "course_completion": "CourseCompletion",
-                "workshop": "Workshop",
-                "hackathon": "Hackathon"
-            }
-            type_name = type_map.get(credential_type) or credential_type.capitalize()
-            type_list.append(type_name)
-
         credential = {
             "@context": VCGenerator.CONTEXT,
             "id": credential_id,
-            "type": type_list,
+            "type": ["VerifiableCredential"],
             "issuer": {
                 "id": issuer_did,
                 "name": issuer_info.get("name", "KLE Technological University")
